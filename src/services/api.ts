77c3372f07074b5970977c8d2067b1a535fd92b5
Pruthvi <dhannapuneni@outlook.com>
2024-09-12 22:20:34 -0400

    remove timeout on getposts.

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,9 +6,7 @@ const API_URL = process.env.REACT_APP_BACKEND_URL + '/reddit';
 const TIMEOUT = 20000;
 
 export const getPosts = ((searchQuery: string, sortBy: string) => 
-  axios.get(`${API_URL}/posts?subreddit=${searchQuery}&categories=${sortBy}&limit=10`, {
-    timeout: TIMEOUT // 20 seconds timeout
-  }));
+  axios.get(`${API_URL}/posts?subreddit=${searchQuery}&categories=${sortBy}&limit=10`));
 
 export const analyzePost = (post_id: string) =>
   axios.get(`${API_URL}/post?post_id=${post_id}`, {
